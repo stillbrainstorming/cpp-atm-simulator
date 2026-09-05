c3fe89486e234faedd1e8b56462a05702ced4b72
Aaqibhafeez Khan <githubaaqibhafeezkhan@gmail.com>
2026-09-05 14:16:59 +0530

    Refactor ATM domain architecture (#7)

--- atm.cpp
+++ atm.cpp
@@ -5,21 +5,25 @@
 
 using namespace std;
 
-class atm
+class AuthenticationService;
+
+class Account
 {
 private:
     long int account_No;
     string name;
-    int PIN;
+    int accessCode;
     double balance;
     string mobile_No;
 
+    friend class AuthenticationService;
+
 public:
-    void setData(long int account_No_a, string name_a, int PIN_a, double balance_a, string mobile_No_a)
+    void setData(long int account_No_a, string name_a, int accessCode_a, double balance_a, string mobile_No_a)
     {
         account_No = account_No_a;
         name = name_a;
-        PIN = PIN_a;
+        accessCode = accessCode_a;
         balance = balance_a;
         mobile_No = mobile_No_a;
     }
@@ -34,11 +38,6 @@ public:
         return name;
     }
 
-    int getPIN()
-    {
-        return PIN;
-    }
-
     double getBalance()
     {
         return balance;
@@ -49,128 +48,234 @@ public:
         return mobile_No;
     }
 
-    void setMobile(string mob_prev, string mob_new)
+    void updateMobile(const string& mobile_No_a)
     {
-        if (mob_prev == mobile_No)
-        {
-            mobile_No = mob_new;
-            cout << endl << "Successfully Updated Mobile No.";
-            _getch();
-        }
-        else
-        {
-            cout << endl << "Incorrect Old Mobile No.";
-            _getch();
-        }
+        mobile_No = mobile_No_a;
     }
 
-    void cashWithDraw(int amount_a)
+    void deductBalance(double amount)
     {
-        if (amount_a <= 0)
-        {
-            cout << endl << "Invalid Withdrawal Amount";
-            _getch();
-        }
-        else if (amount_a <= balance)
-        {
-            balance -= amount_a;
-            cout << endl << "Please Collect Your Cash";
-            cout << endl << "Available Balance: " << balance;
-            _getch();
-        }
-        else
-        {
-            cout << endl << "Invalid Input or Insufficient Balance";
-            _getch();
-        }
+        balance -= amount;
     }
 };
 
-int main()
+enum class WithdrawalResult
 {
-    int choice = 0, enterPIN;
-    long int enterAccountNo;
+    InvalidAmount,
+    InsufficientBalance,
+    Success
+};
 
-    system("cls");
-
-    atm user1;
-    user1.setData(1234567, "Tim", 1111, 45000.90, "9087654321");
-
-    do
+class AuthenticationService
+{
+public:
+    bool authenticate(const Account& account, long int account_No_a, int accessCode_a)
     {
-        system("cls");
+        return account.account_No == account_No_a && account.accessCode == accessCode_a;
+    }
+};
+
+class TransactionService
+{
+public:
+    WithdrawalResult withdraw(Account& account, int amount)
+    {
+        if (amount <= 0)
+        {
+            return WithdrawalResult::InvalidAmount;
+        }
+
+        if (amount > account.getBalance())
+        {
+            return WithdrawalResult::InsufficientBalance;
+        }
+
+        account.deductBalance(amount);
+        return WithdrawalResult::Success;
+    }
+
+    bool updateMobile(Account& account, const string& oldMobileNo, const string& newMobileNo)
+    {
+        if (oldMobileNo != account.getMobileNo())
+        {
+            return false;
+        }
+
+        account.updateMobile(newMobileNo);
+        return true;
+    }
+};
+
+class ATMApplication
+{
+private:
+    Account account;
+    AuthenticationService authenticationService;
+    TransactionService transactionService;
+
+    void waitForInput()
+    {
+        _getch();
+    }
+
+    bool login()
+    {
+        long int enterAccountNo;
+        int enterAccessCode;
 
         cout << endl << "**** Welcome to ATM ****" << endl;
         cout << endl << "Enter Your Account No: ";
         cin >> enterAccountNo;
 
         cout << endl << "Enter PIN: ";
-        cin >> enterPIN;
+        cin >> enterAccessCode;
 
-        if ((enterAccountNo == user1.getAccountNo()) && (enterPIN == user1.getPIN()))
+        if (authenticationService.authenticate(account, enterAccountNo, enterAccessCode))
         {
-            do
-            {
-                int amount = 0;
-                string oldMobileNo, newMobileNo;
+            return true;
+        }
 
-                system("cls");
+        cout << endl << "Invalid Account No. or PIN.";
+        waitForInput();
+        return false;
+    }
 
-                cout << endl << "**** Welcome to ATM ****" << endl;
-                cout << endl << "Select an Option:";
-                cout << endl << "1. Check Balance";
-                cout << endl << "2. Cash Withdraw";
-                cout << endl << "3. Show User Details";
-                cout << endl << "4. Update Mobile No.";
-                cout << endl << "5. Exit" << endl;
-                cin >> choice;
+    void showBalance()
+    {
+        cout << endl << "Your Bank Balance: " << account.getBalance();
+        waitForInput();
+    }
 
-                switch (choice)
-                {
-                case 1:
-                    cout << endl << "Your Bank Balance: " << user1.getBalance();
-                    _getch();
-                    break;
+    void withdrawCash()
+    {
+        int amount = 0;
 
-                case 2:
-                    cout << endl << "Enter the Amount: ";
-                    cin >> amount;
-                    user1.cashWithDraw(amount);
-                    break;
+        cout << endl << "Enter the Amount: ";
+        cin >> amount;
 
-                case 3:
-                    cout << endl << "*** User Details ***";
-                    cout << endl << "Account No : " << user1.getAccountNo();
-                    cout << endl << "Name       : " << user1.getName();
-                    cout << endl << "Balance    : " << user1.getBalance();
-                    cout << endl << "Mobile No  : " << user1.getMobileNo();
-                    _getch();
-                    break;
+        WithdrawalResult result = transactionService.withdraw(account, amount);
 
-                case 4:
-                    cout << endl << "Enter Old Mobile No.: ";
-                    cin >> oldMobileNo;
-                    cout << endl << "Enter New Mobile No.: ";
-                    cin >> newMobileNo;
-                    user1.setMobile(oldMobileNo, newMobileNo);
-                    break;
-
-                case 5:
-                    exit(0);
-
-                default:
-                    cout << endl << "Invalid Input. Please Enter a Valid Option.";
-                }
-
-            } while (1);
+        if (result == WithdrawalResult::Success)
+        {
+            cout << endl << "Please Collect Your Cash";
+            cout << endl << "Available Balance: " << account.getBalance();
+        }
+        else if (result == WithdrawalResult::InvalidAmount)
+        {
+            cout << endl << "Invalid Withdrawal Amount";
         }
         else
         {
-            cout << endl << "Invalid Account No. or PIN.";
-            _getch();
+            cout << endl << "Invalid Input or Insufficient Balance";
         }
 
-    } while (1);
+        waitForInput();
+    }
+
+    void showUserDetails()
+    {
+        cout << endl << "*** User Details ***";
+        cout << endl << "Account No : " << account.getAccountNo();
+        cout << endl << "Name       : " << account.getName();
+        cout << endl << "Balance    : " << account.getBalance();
+        cout << endl << "Mobile No  : " << account.getMobileNo();
+        waitForInput();
+    }
+
+    void updateMobileNo()
+    {
+        string oldMobileNo, newMobileNo;
+
+        cout << endl << "Enter Old Mobile No.: ";
+        cin >> oldMobileNo;
+        cout << endl << "Enter New Mobile No.: ";
+        cin >> newMobileNo;
+
+        if (transactionService.updateMobile(account, oldMobileNo, newMobileNo))
+        {
+            cout << endl << "Successfully Updated Mobile No.";
+        }
+        else
+        {
+            cout << endl << "Incorrect Old Mobile No.";
+        }
+
+        waitForInput();
+    }
+
+    void runSession()
+    {
+        int choice = 0;
+
+        do
+        {
+            system("cls");
+
+            cout << endl << "**** Welcome to ATM ****" << endl;
+            cout << endl << "Select an Option:";
+            cout << endl << "1. Check Balance";
+            cout << endl << "2. Cash Withdraw";
+            cout << endl << "3. Show User Details";
+            cout << endl << "4. Update Mobile No.";
+            cout << endl << "5. Exit" << endl;
+            cin >> choice;
+
+            switch (choice)
+            {
+            case 1:
+                showBalance();
+                break;
+
+            case 2:
+                withdrawCash();
+                break;
+
+            case 3:
+                showUserDetails();
+                break;
+
+            case 4:
+                updateMobileNo();
+                break;
+
+            case 5:
+                exit(0);
+
+            default:
+                cout << endl << "Invalid Input. Please Enter a Valid Option.";
+            }
+
+        } while (1);
+    }
+
+public:
+    void initialize()
+    {
+        account.setData(1234567, "Tim", 1111, 45000.90, "9087654321");
+    }
+
+    void run()
+    {
+        system("cls");
+        initialize();
+
+        do
+        {
+            system("cls");
+
+            if (login())
+            {
+                runSession();
+            }
+
+        } while (1);
+    }
+};
+
+int main()
+{
+    ATMApplication application;
+    application.run();
 
     return 0;
 }
